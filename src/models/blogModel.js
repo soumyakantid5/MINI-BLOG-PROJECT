923251889ba23aa23425fe3559c912958a23f630
Soumya <soumyakantid5@gmail.com>
2022-06-28 13:01:47 +0530

    FINAL PUSH PROJECT 1

---
+++ src/models/blogModel.js
@@ -0,0 +1,53 @@
+const mongoose = require('mongoose');
+const ObjectId = mongoose.Schema.Types.ObjectId
+
+
+//---------------------------------------------------------------//
+
+const blogSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+        lowercase:true
+    },
+    body: {
+        type: String,
+        required: true,
+    },
+    authorId: {
+        type: ObjectId,
+        required: true,
+        ref: "author"
+    },
+    tags: [{
+        type:String,
+        lowercase:true
+    }],
+    category: {
+        type: String,
+        required: true,
+        lowercase:true
+    },
+    subcategory: [{
+        type:String,
+        lowercase:true
+    }],
+    isPublished: {
+        type: Boolean,
+        default: false
+    },
+    publishedAt : {
+        type : Date
+    },
+    isDeleted: {
+        type: Boolean,
+        default: false
+    },
+    deletedAt:{
+        type : Date,
+        // default: ""
+    }
+}, { timestamps: true });
+
+
+module.exports = mongoose.model('blog', blogSchema) 
